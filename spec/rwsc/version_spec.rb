9d70816ed94485aed2dcb5368cb8e6aba8cb9e80
yukio.goto <yukio.goto@mail.rakuten.co.jp>
2009-10-02 16:46:59 +0900

    modify indent .

--- spec/rwsc/version_spec.rb
+++ spec/rwsc/version_spec.rb
@@ -7,7 +7,8 @@ require File.expand_path(File.join(File.dirname(__FILE__),
 
 describe Rwsc::VERSION do
   subject { Rwsc::VERSION::STRING }
+
   it { should == "#{Rwsc::VERSION::MAJOR}." +
-                                      "#{Rwsc::VERSION::MINOR}." +
-                                      "#{Rwsc::VERSION::TINY}" }
+                   "#{Rwsc::VERSION::MINOR}." +
+                   "#{Rwsc::VERSION::TINY}" }
 end
