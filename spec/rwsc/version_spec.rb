3372f2492a032f3bd65999c655d28ffb19ecd8c6
yukio.goto <yukio.goto@mail.rakuten.co.jp>
2009-10-02 16:29:25 +0900

    refactoring version test code .

--- spec/rwsc/version_spec.rb
+++ spec/rwsc/version_spec.rb
@@ -6,9 +6,8 @@ require File.expand_path(File.join(File.dirname(__FILE__),
                                    '..', '..', 'lib', 'rwsc', 'version'))
 
 describe Rwsc::VERSION do
-  it "string version check" do
-    Rwsc::VERSION::STRING.should == "#{Rwsc::VERSION::MAJOR}." +
+  subject { Rwsc::VERSION::STRING }
+  it { should == "#{Rwsc::VERSION::MAJOR}." +
                                       "#{Rwsc::VERSION::MINOR}." +
-                                      "#{Rwsc::VERSION::TINY}"
-  end
+                                      "#{Rwsc::VERSION::TINY}" }
 end
