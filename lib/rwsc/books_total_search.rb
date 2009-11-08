3b6b0dd954987b0ab31c8b36e360db9e3bfa7784
yukio goto <yukio.goto@mail.rakuten.co.jp>
2009-11-08 09:47:33 +0900

    refactoring search sources .
    refactoring item_search.rb and books_total_search.rb .

--- lib/rwsc/books_total_search.rb
+++ lib/rwsc/books_total_search.rb
@@ -1,31 +1,12 @@
 # -*- coding: utf-8 -*-
-require 'rwsc/web/web_client'
+require 'rwsc/searcher'
 require 'rwsc/result_item'
 
 module Rwsc
   # = BooksTotalSearch class
   # this class is BooksTotalSearch API wrapper .
   class BooksTotalSearch < ResultItem
-    # == find item .
-    def self.find(opts)
-      if opts.nil?
-        raise ArgError.new('no options')
-      end
-
-      self.must_items.each do |item|
-        unless opts.include? item
-          raise ArgError.new("#{item.to_s} needed")
-        end
-      end
-
-      opts.each do |key, val|
-        unless accept_items.include? key
-          raise ArgError.new("#{key.to_s} is invalid option")
-        end
-      end
-
-      self.rws_call(opts.merge(self.add_rwsc_opts))
-    end
+    extend Searcher
 
     private
     MUST_ITEMS = [:developerId]
@@ -64,11 +45,6 @@ module Rwsc
     # == get additional Rakuten web service call option
     def self.add_rwsc_opts
       ADD_RWSC_OPTS
-    end
-
-    # == Rakuten web service call function
-    def self.rws_call(opts)
-      Rwsc::Web::WebClient.get_result(opts)
     end
   end
 end
