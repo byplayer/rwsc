eb763da3c45beb6d9ca6e121faff9653920a77d8
byplayer <byplayer100@gmail.com>
2009-12-28 12:37:13 +0900

    fixed file resource leak. read version numbers from file.
    fixed file resource leak, when program read VERSION file.
    read VERSION numbers(MAJOR, MINOR, TINY) from file.

--- spec/rwsc/version_spec.rb
+++ spec/rwsc/version_spec.rb
@@ -4,10 +4,34 @@ require File.expand_path(File.join(File.dirname(__FILE__),
                                    '..', 'spec_helper'))
 require 'rwsc/version'
 
-describe Rwsc::VERSION do
+describe "Rwsc::VERSION::STRING" do
   subject { Rwsc::VERSION::STRING }
 
   it { should == "#{Rwsc::VERSION::MAJOR}." +
                    "#{Rwsc::VERSION::MINOR}." +
                    "#{Rwsc::VERSION::TINY}" }
+
+  it { should == File.open(
+                   File.join(
+                     File.dirname(__FILE__), '..', '..',
+                             'VERSION'), 'r'){ |f| f.gets.chomp } }
+end
+
+describe "Rwsc::VERSION numbers" do
+  before(:each) do
+    @file_version =
+      File.open(
+                File.join(
+                          File.dirname(__FILE__), '..', '..',
+                          'VERSION'), 'r'){ |f| f.gets.chomp }
+    if @file_version =~ /^([0-9]+)\.([0-9]+)\.([0-9]+)/
+      @file_major = $1.to_i
+      @file_minor = $2.to_i
+      @file_tiny = $3.to_i
+    end
+  end
+
+  it { Rwsc::VERSION::MAJOR.should == @file_major }
+  it { Rwsc::VERSION::MINOR.should == @file_minor }
+  it { Rwsc::VERSION::TINY.should == @file_tiny }
 end
