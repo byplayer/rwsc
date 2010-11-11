c6fc50383a0bba036f2e79d9a6e184ada5addc02
Tatsuya Sato <satoryu.1981@gmail.com>
2010-11-12 01:16:38 +0900

    Make spec_helper compatible with RSpec 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-require 'rubygems'
-require 'spec'
+require 'rspec'
 
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 Dir[File.dirname(__FILE__) + "/mock/**/*.rb"].each {|f| require f}
 Dir[File.dirname(__FILE__) + "/matchers/**/*.rb"].each {|f| require f}
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.mock_with :rr
 end
