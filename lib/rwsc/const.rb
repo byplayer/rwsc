4639cb62b54d587a948747d430af971b6a79bbe8
Tatsuya Sato <satoryu.1981@gmail.com>
2010-12-02 02:04:10 +0900

    Changed The Version of Item_Search to 2010-09-15

--- lib/rwsc/const.rb
+++ lib/rwsc/const.rb
@@ -4,7 +4,7 @@ module Rwsc
 
   # = this module is define constatns .
   module CONST
-    API_VERSION = '2009-04-15'
+    API_VERSION = '2010-09-15'
     USER_AGENT = "rwsc/#{Rwsc::VERSION::STRING}"
 
     WS_URL = "http://api.rakuten.co.jp/rws/2.0/rest"
