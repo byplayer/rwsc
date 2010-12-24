b3483ca1c067ccd96f8a011a9152f4a8d21c1485
Tatsuya Sato <satoryu.1981@gmail.com>
2010-12-25 02:53:29 +0900

    Added an example script to explain how to use books total search

---
+++ example/rwsc/books_total_search_example.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+require 'rwsc'
+require 'optparse'
+
+options = {}
+opt = OptionParser.new
+opt.on('-d DEVID', '--developerid DEVID'){|id| options[:developerId] = id}
+opt.on('-a AFFID', '--affiliateid DEVID'){|id| options[:affiliateId] = id}
+opt.on('-k KEYWORD', '--keyword KEYWORD'){|keyword| options[:keyword] = keyword}
+opt.parse!(ARGV)
+
+result = Rwsc::BooksTotalSearch.find(options)
+result.items.each do |item|
+  puts item.title
+end
+
