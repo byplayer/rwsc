5f4af908e02218471ea66c0be68398a99a9244ea
Tatsuya Sato <satoryu.1981@gmail.com>
2010-12-25 00:34:04 +0900

    Modified Request URL.

--- lib/rwsc/const.rb
+++ lib/rwsc/const.rb
@@ -7,6 +7,6 @@ module Rwsc
     API_VERSION = '2010-09-15'
     USER_AGENT = "rwsc/#{Rwsc::VERSION::STRING}"
 
-    WS_URL = "http://api.rakuten.co.jp/rws/2.0/rest"
+    WS_URL = "http://api.rakuten.co.jp/rws/3.0/rest"
   end
 end
